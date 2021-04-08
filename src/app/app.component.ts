f563b55f7b0d539772ed273f5e62c4039ddcc701
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-04-07 22:03:50 -0300

    password generator with bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'password-generator';
+  length: number = 0;
+  password: string = "";
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onChangeLength(target: EventTarget | null) {
+    if (!target) return
+    const parsedInt = parseInt((target as HTMLInputElement).value)
+    if (isNaN(parsedInt)) return
+    this.length = parsedInt;
+  }
+  onButtonClicked() {
+    const numbers = "1234567890"
+    const letters = "abcdefghijklmnopqrstuvwxyz"
+    const symbols = "!$%&@()=?+#*"
+    let validChars = ""
+    if (this.includeLetters) validChars += letters
+    if (this.includeNumbers) validChars += numbers
+    if (this.includeSymbols) validChars += symbols
+
+    if (!validChars) {
+      this.password = "";
+      return;
+    }
+    let generatedPassword = ""
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
